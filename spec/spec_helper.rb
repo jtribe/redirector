3fc4a787271c746235553ac334ce445591066a34
Dougal MacPherson <dougal@jtribe.com.au>
2013-12-05 07:43:15 +1100

    Adding in rspec

---
+++ spec/spec_helper.rb
@@ -0,0 +1,15 @@
+$:.push File.join(File.dirname(__FILE__), '..', 'lib')
+
+# require 'rack/mock'
+require 'redirector/app'
+
+
+RSpec.configure do |config|
+  config.color
+  config.order = 'random'
+  
+  config.before(:all) do
+    ENV['PWD'] = File.join(File.dirname(__FILE__), 'sample')
+  end
+  
+end
